3ad51d0116ccb2dba0ad3d829b71f84bc7922130
Sofia Jonsson <jonssonsofia93@gmail.com>
2019-02-06 19:19:11 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,3 +17,18 @@ def find_the_cheese# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
+
+
+def line (array)
+  if array.length >= 1
+    array2 = []
+    counter = 1 
+    array.each do |name|
+        array2.push("#{counter}. #{name}")
+        counter += 1 
+      end 
+      puts "The line is currently: #{array2.join(" ")}"
+    else
+      puts "The line is currently empty."
+    end
+  end
