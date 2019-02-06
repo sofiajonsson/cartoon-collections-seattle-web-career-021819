9ab59b187dd88a2208c7bae0b2ec2366b8363e5e
Sofia Jonsson <jonssonsofia93@gmail.com>
2019-02-06 19:45:14 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -30,7 +30,7 @@ def long_planeteer_calls(array)# code an argument here
   array.any? do |array|
     array.length>4
 end
-
+end
 def find_the_cheese# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
